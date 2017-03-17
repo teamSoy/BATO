d434d7d16074c436af7f981312fb88c1b966a3df
Niel Jon Carl Aguel <nieljoncarlaguel@icloud.com>
2017-03-17 22:26:41 +0800

    Update: Camera Upload

--- app/src/main/java/net/aguel/bato/Fragment/Fragment_Camera.java
+++ app/src/main/java/net/aguel/bato/Fragment/Fragment_Camera.java
@@ -1,5 +1,6 @@
 package net.aguel.bato.Fragment;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
@@ -11,6 +12,7 @@ import android.provider.MediaStore;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.SharedPreferencesCompat;
+import android.util.Base64;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -23,14 +25,26 @@ import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.VideoView;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 import net.aguel.bato.R;
 
 import org.w3c.dom.Text;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Hashtable;
 import java.util.Locale;
+import java.util.Map;
 
 import static android.app.Activity.RESULT_CANCELED;
 import static android.app.Activity.RESULT_OK;
@@ -48,6 +62,7 @@ public class Fragment_Camera extends Fragment
 
     // directory name to store captured images and videos
     private static final String IMAGE_DIRECTORY_NAME = "BATO Captured Images";
+    private static String name;
 
     private Uri fileUri; // file url to store image/video
 
@@ -93,6 +108,7 @@ public class Fragment_Camera extends Fragment
             @Override
             public void onClick(View v) {
                 // send file to database
+                uploadImage();
 
             }
         });
@@ -207,9 +223,8 @@ public class Fragment_Camera extends Fragment
             // images
             options.inSampleSize = 8;
 
-            final Bitmap bitmap = BitmapFactory.decodeFile(fileUri.getPath(),
+            bitmap = BitmapFactory.decodeFile(fileUri.getPath(),
                     options);
-
             imgPreview.setImageBitmap(bitmap);
         } catch (NullPointerException e) {
             e.printStackTrace();
@@ -277,7 +292,79 @@ public class Fragment_Camera extends Fragment
         } else {
             return null;
         }
-
+        name = String.valueOf(mediaFile);
         return mediaFile;
     }
+
+    /**
+     *
+     *      Upload Part
+     *
+     * **/
+
+    private Bitmap bitmap;
+
+
+    private String UPLOAD_URL ="http://192.168.3.2/upload.php";
+
+    private String KEY_IMAGE = "image";
+    private String KEY_NAME = "name";
+
+    public String getStringImage(Bitmap bmp){
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bmp.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        byte[] imageBytes = baos.toByteArray();
+        String encodedImage = Base64.encodeToString(imageBytes, Base64.DEFAULT);
+        return encodedImage;
+    }
+
+    private void uploadImage(){
+        //Showing the progress dialog
+        final ProgressDialog loading = ProgressDialog.show(getActivity(),"Uploading...","Please wait...",false,false);
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, UPLOAD_URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String s) {
+                        //Disimissing the progress dialog
+                        loading.dismiss();
+                        //Showing toast message of the response
+                        Toast.makeText(getActivity(), s , Toast.LENGTH_LONG).show();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError volleyError) {
+                        //Dismissing the progress dialog
+                        loading.dismiss();
+
+                        //Showing toast
+                        Toast.makeText(getActivity(), volleyError.getMessage().toString()+"", Toast.LENGTH_LONG).show();
+                    }
+                }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                //Converting Bitmap to String
+                String image = getStringImage(bitmap);
+
+                //Getting Image Name
+
+                //Creating parameters
+                Map<String,String> params = new Hashtable<String, String>();
+
+                //Adding parameters
+                params.put(KEY_IMAGE, image);
+                params.put(KEY_NAME, name);
+
+                //returning parameters
+                return params;
+            }
+        };
+
+        //Creating a Request Queue
+        RequestQueue requestQueue = Volley.newRequestQueue(getActivity());
+
+        //Adding request to the queue
+        requestQueue.add(stringRequest);
+    }
+
 }
